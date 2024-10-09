3c8a45309b4155cbd3b3a0dc8d5a6fafa6f31324
Xavi Subirà <el.temple@gmail.com>
2024-10-09 17:33:58 +0000

    Session Table for SQLiteSessionProvider

--- db/config.ts
+++ db/config.ts
@@ -1,14 +1,24 @@
 import { defineDb, defineTable, column } from 'astro:db';
 
+const Users = defineTable({
+  columns: {
+    email: column.text({primaryKey: true}),
+    fullName: column.text(),
+    password: column.text({optional: true}),
+  }
+})
+
+const Sessions = defineTable({
+  columns: {
+    sessionId: column.text({primaryKey: true}),
+    userId: column.text({references: () => Users.columns.email})
+  }
+})
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
-    Users : defineTable({
-      columns: {
-        email: column.text({primaryKey: true}),
-        fullName: column.text(),
-        password: column.text({optional: true}),
-      }
-    }),
+    Users,
+    Sessions,
   }
 });
